cb024a60bab5b960533cb17e8e1cde29ac4c3394
Matthias Derer <matthias.derer@esono.de>
2016-06-07 23:08:31 +0200

    remove all old test and cover them in the table driven test

--- parser_test.go
+++ parser_test.go
@@ -21,6 +21,10 @@ func TestCases(t *testing.T) {
 		ExpectedValues             []string
 	}{
 		{"foo: bar", "foo", "foo", []string{"bar"}},
+		{"foo: bar\nfoo: bar2", "foo", "foo", []string{"bar", "bar2"}},
+		{"foo: bar\nfoo: bar2", "bar", "bar", []string{}},
+		{"foo: bar\nbar: bar\nfoo: bar2", "foo", "foo", []string{"bar", "bar2"}},
+		{"\n\nfoo: bar\n\n\n\nfoo: bar2", "foo", "foo", []string{"bar", "bar2"}},
 	}
 
 	for _, c := range cases {
@@ -41,60 +45,5 @@ func TestCases(t *testing.T) {
 				}
 			}
 		}
-
-	}
-}
-
-func TestSimpleEntry(t *testing.T) {
-	data := "foo: bar"
-	p := SimpleParser{}
-	parsedData, err := p.parse("foo", strings.NewReader(data))
-
-	if err != nil {
-		t.Fatal("Unexpected error")
-	}
-
-	if parsedData.Title != "foo" {
-		t.Fatal("wrong title")
-	}
-
-	if len(parsedData.Values) == 0 {
-		t.Fatal("No data")
-	}
-
-	if parsedData.Values[0] != "bar" {
-		t.Fatal("Wrong data")
-	}
-}
-
-func TestSelectiveEntry(t *testing.T) {
-	data := "foo: bar\nbar: baz"
-	p := SimpleParser{}
-	parsedData, _ := p.parse("bar", strings.NewReader(data))
-
-	if parsedData.Title != "bar" {
-		t.Fatal("wrong title")
-	}
-
-	if len(parsedData.Values) != 1 {
-		t.Fatal("Wrong amount of data")
-	}
-
-	if parsedData.Values[0] != "baz" {
-		t.Fatal("Wrong data")
-	}
-}
-
-func TestmultipleEntries(t *testing.T) {
-	data := "bar: baz1\nfoo: bar\nbar: baz2"
-	p := SimpleParser{}
-	parsedData, _ := p.parse("bar", strings.NewReader(data))
-
-	if len(parsedData.Values) != 2 {
-		t.Fatal("Wrong amount of data")
-	}
-
-	if parsedData.Values[0] != "baz1" || parsedData.Values[1] != "baz2" {
-		t.Fatal("Wrong data")
 	}
 }
